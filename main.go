4b38bb14852691de5e0c79ce9c275cd49e82f743
Thomas Fossati <thomas.fossati@arm.com>
2023-01-25 18:53:21 +0000

    barebones thingy
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	pem, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatalf("reading PEM file from stdin: %s", err)
+	}
+
+	k, err := jwk.ParseKey(pem, jwk.WithPEM(true))
+	if err != nil {
+		log.Fatalf("parsing PEM file into JWK: %s", err)
+	}
+
+	buf, err := json.MarshalIndent(k, "", "  ")
+	if err != nil {
+		log.Fatalf("failed to marshal key into JSON: %s", err)
+	}
+
+	fmt.Println(string(buf))
+}
